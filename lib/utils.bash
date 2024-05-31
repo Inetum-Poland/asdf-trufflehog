a4ec73d7071158f3d67d6d17bffd543116904245
Aam <amadeusz.kryze@pm.me>
2024-05-31 21:06:52 +0200

    feat(asdf): implement better downloading mechanisms #2

--- lib/utils.bash
+++ lib/utils.bash
@@ -18,6 +18,32 @@ if [ -n "${GITHUB_API_TOKEN:-}" ]; then
 	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
 fi
 
+platform=""
+_platform="$(uname -s)"
+case "${_platform,,}" in
+	darwin*) platform="darwin" ;;
+	linux*) platform="linux" ;;
+	# freebsd*) platform="freebsd" ;;
+	# netbsd*) platform="netbsd" ;;
+	# openbsd*) platform="openbsd" ;;
+	*) fail "Unsupported platform" ;;
+esac
+
+architecture=""
+_architecture="$(uname -m)"
+case "${_architecture,,}" in
+	aarch64* | arm64) architecture="arm64" ;;
+	x86_64*) architecture="amd64" ;;
+	# armv5* | armv6* | armv7*) architecture="arm" ;;
+	# i686*) architecture="386" ;;
+	# ppc64le*) architecture="ppc64le" ;;
+	# ppc64*) architecture="ppc64" ;;
+	# ppc*) architecture="ppc" ;;
+	# mipsel*) architecture="mipsle" ;;
+	# mips*) architecture="mips" ;;
+	*) fail "Unsupported architecture" ;;
+esac
+
 sort_versions() {
 	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
 		LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
@@ -39,48 +65,37 @@ list_all_versions() {
 # trufflehog_3.77.0_linux_amd64.tar.gz
 # trufflehog_3.77.0_linux_arm64.tar.gz
 download_release() {
-	local version filename url
-	version="$1"
-	filename="$2"
-
-	local platform
-
-  case "${(uname -s),,}" in
-    darwin*) platform="darwin" ;;
-    linux*) platform="linux" ;;
-    # freebsd*) platform="freebsd" ;;
-    # netbsd*) platform="netbsd" ;;
-    # openbsd*) platform="openbsd" ;;
-    *) fail "Unsupported platform" ;;
-  esac
-
-  local architecture
-
-  case "${(uname -m),,}" in
-    aarch64* | arm64) architecture="arm64" ;;
-    x86_64*) architecture="amd64" ;;
-    # armv5* | armv6* | armv7*) architecture="arm" ;;
-    # i686*) architecture="386" ;;
-    # ppc64le*) architecture="ppc64le" ;;
-    # ppc64*) architecture="ppc64" ;;
-    # ppc*) architecture="ppc" ;;
-    # mipsel*) architecture="mipsle" ;;
-    # mips*) architecture="mips" ;;
-    *) fail "Unsupported architecture" ;;
-  esac
+	local url
+	local version="$1"
+	local release_file="${TOOL_NAME}_${ASDF_INSTALL_VERSION}_${platform}_${architecture}.tar.gz"
+	local checksum_release_file="${TOOL_NAME}_${version}_checksums.txt"
 
 	# /releases/download/v3.77.0/trufflehog_3.77.0_darwin_arm64.tar.gz
-	url="${GH_REPO}/releases/download/v${version}/${TOOL_NAME}_${version}_${platform}_${architecture}.tar.gz"
+	url="${GH_REPO}/releases/download/v${version}/${release_file}"
+
 	# /releases/download/v3.77.0/trufflehog_3.77.0_checksums.txt
-	sha_url="${GH_REPO}/releases/download/v${version}/${TOOL_NAME}_${version}_checksums.txt"
+	sha_url="${GH_REPO}/releases/download/v${version}/${checksum_release_file}"
 
-	echo "* Downloading $TOOL_NAME release $version..."
-	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
+	echo "* Downloading ${TOOL_NAME} release ${version}..."
+	curl "${curl_opts[@]}" -o "${ASDF_DOWNLOAD_PATH}/${release_file}" -C - "$url" || fail "Could not download ${url}"
 
-	echo "* Downloading $TOOL_NAME release checksum $version..."
-	curl "${curl_opts[@]}" -o "$filename.sha256" -C - "$sha_url" || fail "Could not download $sha_url"
+	echo "* Downloading ${TOOL_NAME} release checksum ${version}..."
+	curl "${curl_opts[@]}" -o "${ASDF_DOWNLOAD_PATH}/${checksum_release_file}" -C - "${sha_url}" || fail "Could not download ${sha_url}"
 
-	sha256sum -c "$filename.sha256" || fail "Could not verify checksum for $filename"
+	pushd "${ASDF_DOWNLOAD_PATH}"
+	grep "${release_file}" "${checksum_release_file}" | sha256sum -c || fail "Could not verify checksum for $filename"
+	rm "${checksum_release_file}"
+	popd
+}
+
+unpack_release() {
+	local install_path="$1"
+	local release_file="${TOOL_NAME}_${ASDF_INSTALL_VERSION}_${platform}_${architecture}.tar.gz"
+
+	tar -xzf "${ASDF_DOWNLOAD_PATH}/${release_file}" -C "${install_path}" --strip-components=0 || fail "Could not extract ${release_file}"
+
+	# Remove the tar.gz file since we don't need to keep it
+	rm "${ASDF_DOWNLOAD_PATH}/${release_file}"
 }
 
 install_version() {
@@ -102,7 +117,7 @@ install_version() {
 
 		echo "$TOOL_NAME $version installation was successful!"
 	) || (
-		# rm -rf "$install_path"
+		rm -rf "$install_path"
 		fail "An error occurred while installing $TOOL_NAME $version."
 	)
 }
